25fc9523f7f9d46a894c0af4987e1ec334380db5
FaziletKosure <faziletkosure1@gmail.com>
2021-01-10 19:22:39 +0100

    add

--- clarusway/fscohort_api/urls.py
+++ clarusway/fscohort_api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.student_list),
     path('<int:pk>/', views.student_detail),
     path('home-api', views.home_api),
+    path("list-api/", views.student_list_api),
+    path("create-api/", views.student_create_api),
 ]
--- clarusway/fscohort_api/views.py
+++ clarusway/fscohort_api/views.py
@@ -4,6 +4,9 @@ from rest_framework.decorators import api_view
 from .serializers import StudentSerializer
 from fscohort.models import Student
 from django.http import JsonResponse
+from django.core.serializers import serialize
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 def home_api(request):
@@ -58,3 +61,61 @@ def student_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
+# def student_list_api(request):
+#     if request.method == "GET":
+#         students = Student.objects.all()
+#         student_count = Student.objects.count()
+
+#         student_list = []
+#         for student in students: # mevlüt, henry
+#             student_list.append({
+#                 "fisrtname": student.first_name,
+#                 "lastname": student.last_name,
+#                 "number": student.number
+#             })
+#         print(student_list)
+
+#         data = {
+#             "students": student_list,
+#             "count": student_count
+#         }
+#         return JsonResponse(data)
+
+
+def student_list_api(request):
+    if request.method == "GET":
+        students = Student.objects.all()
+        student_count = Student.objects.count()
+        student_data = serialize("python", students)
+        print(student_data)
+        data = {
+            "students": student_data,
+            "count": student_count
+        }
+        return JsonResponse(data)
+
+
+@csrf_exempt
+def student_create_api(request):
+    if request.method == "POST":
+        post_body = json.loads(request.body)
+        print(post_body)
+        print(type(post_body))
+
+        name = post_body.get("first_name")
+        lastname = post_body.get("last_name")
+        number = post_body.get("number")
+
+        student_data = {
+            "first_name": name,
+            "last_name": lastname,
+            "number": number
+        }
+        # print(**student_data)
+
+        student_obj = Student.objects.create(
+            **student_data)  # "firts_name": name, "last_name": lastname,
+        data = {
+            "message": f"Student {student_obj.first_name} created succesfully "
+        }
+        return JsonResponse(data, status=201)
